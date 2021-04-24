ff451da2f5b47da1228dd2d10470fcca7e594a1f
adebayus <adebayus17@gmail.com>
2021-04-24 19:57:28 +0700

    [Method/Verb Request][fix]

--- server.js
+++ server.js
@@ -7,13 +7,21 @@ const http = require("http");
  */
 
 const requestListerner = (request, response) => {
-	const { method } = request;
-	console.log(method);
-	response.end(method);
-
 	response.setHeader("Content-Type", "text/html");
 	response.statusCode = 200;
-	response.end("<h1> Halo Http Server!</h1>");
+	const { method } = request;
+	if (method === "GET") {
+		response.end("<h1>Hello</h1>");
+	}
+	if (method === "POST") {
+		response.end("<h1>Hai</h1>");
+	}
+	if (method === "PUT") {
+		response.end("<h1>Bonjour</h1>");
+	}
+	if (method === "DELETE") {
+		response.end("<h1>Salam</h1>");
+	}
 };
 
 const serve = http.createServer(requestListerner);
